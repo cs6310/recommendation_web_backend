899a237f7713278f6cc6d74376254a9556581de3
ahenderson44 <ahenderson44@gatech.edu>
2015-11-17 19:30:12 -0500

    Added person methods. Fixed package names

--- app/models/Person.java
+++ app/models/Person.java
@@ -1,22 +1,34 @@
-package controllers;
+package models;
 
 import java.util.ArrayList;
 
 public class Person
 {
-  private int id;
+  private String first_name;
+  private String last_name;
+  private int UID;
+  private String username;
 
   Person(int id) {
     // we are not actually going to implement the person class
     // so a constructor for it should blow up
-    setId(id);
+    this.UID = id;
   }
-
-  public int getId() {
-    return this.id;
+  
+  public String get_first_name() {
+	  return first_name;
   }
-
-  public void setId(int id) {
-    this.id = id;
+  
+  public String get_last_name() {
+	  return last_name;
   }
+  
+  public int get_UID() {
+	  return UID;
+  }
+  
+  public String get_username() {
+	  return username;
+  }
+  
 }
