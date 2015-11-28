a71cc5601b27efd51ff6ef9d55b34949913ac27f
Nate Holland <nateh@spiceworks.com>
2015-11-28 12:51:50 -0600

    adding comments for the person class

--- app/models/Person.java
+++ app/models/Person.java
@@ -1,5 +1,9 @@
 package models;
 
+/* The Person class is an abstract class that was designed to be inherited from
+  * by all of the different types of people in our system. The basic logic about
+  * a person is contained in this class.
+*/
 public class Person
 {
   private String first_name;
@@ -8,25 +12,35 @@ public class Person
   private String username;
 
   Person(int id) {
-    // we are not actually going to implement the person class
-    // so a constructor for it should blow up
     this.UID = id;
   }
-  
+
+  /* getter for a persons first name
+   * returns a string
+   */
   public String get_first_name() {
 	  return first_name;
   }
-  
+
+  /* getter for a persons last name
+   * returns a string
+   */
   public String get_last_name() {
 	  return last_name;
   }
-  
+
+  /* getter for a persons unique ID number
+   * returns an int
+   */
   public int get_UID() {
 	  return UID;
   }
-  
+
+  /* getter for a persons username
+   * returns an String
+   */
   public String get_username() {
 	  return username;
   }
-  
+
 }
